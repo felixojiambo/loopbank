353e11dfc7c01f92ea50bf2f7fafa47b6461503f
Felix Ojiambo <felixojiamboe@gmail.com>
2024-05-01 06:44:44 -0700

    user model

--- src/main/java/com/zep/bankingkafka/models/User.java
+++ src/main/java/com/zep/bankingkafka/models/User.java
@@ -1,11 +1,22 @@
 package com.zep.bankingkafka.models;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private  Long id;
     private  String firstName;
     private  String lastName;
@@ -23,5 +34,4 @@ public class User {
     private LocalDateTime createdAt;
     @UpdateTimestamp
     private  LocalDateTime modifiedAt;
-
 }
