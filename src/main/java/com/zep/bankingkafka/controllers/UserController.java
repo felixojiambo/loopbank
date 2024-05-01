e0561ecbafd804a16ee78d1dd9baac2684ebc782
Felix Ojiambo <felixojiamboe@gmail.com>
2024-05-01 12:07:05 -0700

    balance and name enquiry endpoint

--- src/main/java/com/zep/bankingkafka/controllers/UserController.java
+++ src/main/java/com/zep/bankingkafka/controllers/UserController.java
@@ -1,13 +1,11 @@
 package com.zep.bankingkafka.controllers;
 
 import com.zep.bankingkafka.dtos.BankResponse;
+import com.zep.bankingkafka.dtos.EnquiryRequest;
 import com.zep.bankingkafka.dtos.UserRequest;
 import com.zep.bankingkafka.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/user")
@@ -17,5 +15,15 @@ public class UserController {
     @PostMapping("/create")
     public BankResponse createAccount(@RequestBody UserRequest userRequest){
         return  userService.createAccount(userRequest);
+    }
+    @GetMapping("/balanceEnquiry")
+    public  BankResponse balanceEnquiry(@RequestBody EnquiryRequest request){
+        return  userService.balanceEnquiry(request);
+    }
+
+    @GetMapping("/nameEnquiry")
+    public  String nameEnquiry(@RequestBody EnquiryRequest request)
+    {
+        return  userService.nameEnquiry(request);
     }
 }
