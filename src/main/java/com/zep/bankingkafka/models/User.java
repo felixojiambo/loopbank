c4365b2a0973ccf8e5e9e97461d71ca0bb05479b
Felix Ojiambo <felixojiamboe@gmail.com>
2024-05-01 09:59:20 -0700

    mail dto

--- src/main/java/com/zep/bankingkafka/models/User.java
+++ src/main/java/com/zep/bankingkafka/models/User.java
@@ -1,14 +1,12 @@
 package com.zep.bankingkafka.models;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
