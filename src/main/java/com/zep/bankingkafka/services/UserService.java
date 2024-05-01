c3e7338127d98df9c4879fa5ebf9e48740b685b4
Felix Ojiambo <felixojiamboe@gmail.com>
2024-05-01 10:07:56 -0700

    mail service sending

--- src/main/java/com/zep/bankingkafka/services/UserService.java
+++ src/main/java/com/zep/bankingkafka/services/UserService.java
@@ -1,8 +1,6 @@
 package com.zep.bankingkafka.services;
-
 import com.zep.bankingkafka.dtos.BankResponse;
 import com.zep.bankingkafka.dtos.UserRequest;
-
 public interface UserService {
     BankResponse createAccount(UserRequest userRequest);
 }
