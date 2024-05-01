b64fda965a71e4396d391df9366fe52c6897c82f
Felix Ojiambo <felixojiamboe@gmail.com>
2024-05-01 06:41:39 -0700

    initial setup

---
+++ src/main/java/com/zep/bankingkafka/BankingkafkaApplication.java
@@ -0,0 +1,14 @@
+package com.zep.bankingkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingkafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingkafkaApplication.class, args);
+		System.out.println("Zep Loop");
+	}
+
+}
---
+++ src/main/java/com/zep/bankingkafka/models/User.java
@@ -0,0 +1,27 @@
+package com.zep.bankingkafka.models;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+@Data
+public class User {
+    private  Long id;
+    private  String firstName;
+    private  String lastName;
+    private  String otherName;
+    private String gender;
+    private  String address;
+    private  String county;
+    private  String accountNumber;
+    private BigDecimal accountBalance;
+    private  String email;
+    private  String phoneNumber;
+    private String  alternativePhoneNumber;
+    private  String status;
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+    @UpdateTimestamp
+    private  LocalDateTime modifiedAt;
+
+}
