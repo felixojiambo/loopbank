66ebf426ee34facaf2c69b400d45da629a033693
Felix Ojiambo <felixojiamboe@gmail.com>
2024-05-02 05:21:57 -0700

    set operations and api response

--- src/main/java/com/zep/bankingkafka/controllers/UserController.java
+++ src/main/java/com/zep/bankingkafka/controllers/UserController.java
@@ -1,8 +1,10 @@
 package com.zep.bankingkafka.controllers;
+
 import com.zep.bankingkafka.dtos.*;
 import com.zep.bankingkafka.services.UserService;
-import io.swagger.annotations.ApiResponse;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -13,35 +15,64 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
     @Autowired
     UserService userService;
+
     @Operation(
             summary = "Create New User Account",
-            description = "Creating new user  and assigning account number"
+            description = "Creating new user and assigning account number"
     )
-    @ApiResponse()
+    @ApiResponse(responseCode = "201", description = "Http Status 201 Created")
     @PostMapping("/create")
     public BankResponse createAccount(@RequestBody UserRequest userRequest){
-        return  userService.createAccount(userRequest);
-    }
-    @GetMapping("/balanceEnquiry")
-    public  BankResponse balanceEnquiry(@RequestBody EnquiryRequest request){
-        return  userService.balanceEnquiry(request);
+        return userService.createAccount(userRequest);
     }
 
-    @GetMapping("/nameEnquiry")
-    public  String nameEnquiry(@RequestBody EnquiryRequest request)
-    {
-        return  userService.nameEnquiry(request);
+    @Operation(
+            summary = "Balance Enquiry",
+            description = "Enquire the balance of a user account"
+    )
+    @ApiResponse(responseCode = "200", description = "Http Status 200 OK")
+    @GetMapping("/balanceEnquiry")
+    public BankResponse balanceEnquiry(@RequestBody EnquiryRequest request){
+        return userService.balanceEnquiry(request);
     }
+
+    @Operation(
+            summary = "Name Enquiry",
+            description = "Enquire the name of a user account"
+    )
+    @ApiResponse(responseCode = "200", description = "Http Status 200 OK")
+    @GetMapping("/nameEnquiry")
+    public String nameEnquiry(@RequestBody EnquiryRequest request){
+        return userService.nameEnquiry(request);
+    }
+
+    @Operation(
+            summary = "Credit Account",
+            description = "Credit an amount to a user account"
+    )
+    @ApiResponse(responseCode = "200", description = "Http Status 200 OK")
     @PostMapping("/credit")
-    public  BankResponse creditAccount(@RequestBody CreditDebitRequest request){
+    public BankResponse creditAccount(@RequestBody CreditDebitRequest request){
         return userService.creditAccount(request);
     }
+
+    @Operation(
+            summary = "Debit Account",
+            description = "Debit an amount from a user account"
+    )
+    @ApiResponse(responseCode = "200", description = "Http Status 200 OK")
     @PostMapping("/debit")
-    public  BankResponse debitAccount(@RequestBody CreditDebitRequest request){
+    public BankResponse debitAccount(@RequestBody CreditDebitRequest request){
         return userService.debitAccount(request);
     }
+
+    @Operation(
+            summary = "Transfer Funds",
+            description = "Transfer funds between user accounts"
+    )
+    @ApiResponse(responseCode = "200", description = "Http Status 200 OK")
     @PostMapping("/transfer")
-    public  BankResponse transfer(@RequestBody TransferRequest request){
-        return  userService.transfer(request);
+    public BankResponse transfer(@RequestBody TransferRequest request){
+        return userService.transfer(request);
     }
 }
