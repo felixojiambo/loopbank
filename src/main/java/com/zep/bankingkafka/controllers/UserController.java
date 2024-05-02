9e614705237708f34c4b836a97965f6da683cb94
Felix Ojiambo <felixojiamboe@gmail.com>
2024-05-02 01:00:50 -0700

    transfer endpoint

--- src/main/java/com/zep/bankingkafka/controllers/UserController.java
+++ src/main/java/com/zep/bankingkafka/controllers/UserController.java
@@ -1,8 +1,5 @@
 package com.zep.bankingkafka.controllers;
-import com.zep.bankingkafka.dtos.BankResponse;
-import com.zep.bankingkafka.dtos.CreditDebitRequest;
-import com.zep.bankingkafka.dtos.EnquiryRequest;
-import com.zep.bankingkafka.dtos.UserRequest;
+import com.zep.bankingkafka.dtos.*;
 import com.zep.bankingkafka.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -33,5 +30,9 @@ public class UserController {
     @PostMapping("/debit")
     public  BankResponse debitAccount(@RequestBody CreditDebitRequest request){
         return userService.debitAccount(request);
+    }
+    @PostMapping("/transfer")
+    public  BankResponse transfer(@RequestBody TransferRequest request){
+        return  userService.transfer(request);
     }
 }
