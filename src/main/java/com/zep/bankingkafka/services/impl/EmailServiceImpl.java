87bc074be2c2de7dabf11b07b8f5505476f9c986
Felix Ojiambo <felixojiamboe@gmail.com>
2024-05-02 04:06:59 -0700

    service annotation on mail service

--- src/main/java/com/zep/bankingkafka/services/impl/EmailServiceImpl.java
+++ src/main/java/com/zep/bankingkafka/services/impl/EmailServiceImpl.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
 
+@Service
 public class EmailServiceImpl implements EmailService {
     @Autowired
     private JavaMailSender javaMailSender;
