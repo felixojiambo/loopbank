374440455853690bcbffac7487791ad18b5e1d8b
Felix Ojiambo <felixojiamboe@gmail.com>
2024-05-02 22:26:31 -0700

    inherit exceptions in controller

--- src/main/java/com/zep/bankingkafka/controllers/TransactionController.java
+++ src/main/java/com/zep/bankingkafka/controllers/TransactionController.java
@@ -1,5 +1,6 @@
 package com.zep.bankingkafka.controllers;
 
+import com.itextpdf.text.DocumentException;
 import com.zep.bankingkafka.models.Transaction;
 import com.zep.bankingkafka.services.impl.BankStatement;
 import lombok.AllArgsConstructor;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.FileNotFoundException;
 import java.util.List;
 
 @RestController
@@ -17,7 +19,7 @@ public class TransactionController {
   private BankStatement bankStatement;
 
   @GetMapping("/generate")
-  public List<Transaction> generateBankStatement(@RequestParam String accountNumber,@RequestParam String startDate,@RequestParam String endDate){
+  public List<Transaction> generateBankStatement(@RequestParam String accountNumber,@RequestParam String startDate,@RequestParam String endDate) throws DocumentException, FileNotFoundException {
       return  bankStatement.generateStatement(accountNumber,startDate,endDate);
 
   }
